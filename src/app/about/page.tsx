697439e5268df35f1a38e33c9acc3c66e9c58549
ramohin1236 <mohinr26@gmail.com>
2025-12-28 09:04:45 +0600

    about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import images from "../../../public/hero 2-Photoroom.svg";
 import aboutImages from "../../../public/about.svg";
 import Image from "next/image";
+import mission from "../../../public/mission.svg"
+import vision from "../../../public/vission.svg"
+import values from "../../../public/values.svg"
 
 const About = () => {
   return (
@@ -34,9 +37,10 @@ const About = () => {
         </div>
       </div>
 
-      {/* content */}
+      <div className="container mx-auto px-6">
+         {/* content */}
       <div className="container mx-auto px-8 py-10 md:py-28">
-        <div className="flex justify-between items-center gap-24">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-5 md:gap-24">
           {/* left */}
           <div>
             <Image
@@ -44,25 +48,82 @@ const About = () => {
               alt="about image"
               width={600}
               height={400}
+              className="w-full lg:w-[450px]"
             />
           </div>
 
           {/* right  */}
-          <div className="lg:w-[60%] flex flex-col gap-4 md:gap-7">
+          <div className="w-full lg:w-[60%] flex flex-col gap-4 md:gap-7">
             <div>
               <h2 className="text-2xl lg:text-[40px] leading-[120%] font-semibold">
                 When an insurance <span className="text-brand"> claim</span>  feels unclear, knowing where you stand matters. 
               </h2>
             </div>
             <div className="flex flex-col gap-4">
-              <p className="default-list-text">
+              <p className="default-list-text leading-normal!">
                Claimly has been created by Australians for Australians with over 10 years’ combined experience in the insurance industry, across claims handling, internal dispute resolution, and external disputes through AFCA. We understand how insurers assess claims, what evidence they rely on, and where misunderstandings commonly arise.
               </p>
-              <p className="default-list-text">Claimly helps you make sense of your situation by turning your documents and answers into a clear, plain-English report explaining how a claim may be viewed, what factors usually matter, and what options are commonly available next.</p>
-              <p className="default-list-text">Claimly provides general information only - not legal or financial advice.</p>
+              <p className="default-list-text leading-normal!">Claimly helps you make sense of your situation by turning your documents and answers into a clear, plain-English report explaining how a claim may be viewed, what factors usually matter, and what options are commonly available next.</p>
+              <p className="default-list-text leading-normal!">Claimly provides general information only - not legal or financial advice.</p>
             </div>
           </div>
         </div>
+      </div>
+
+      {/* bottom content */}
+
+      <div className="px-8 mt-10 mb-10 lg:mb-28">
+         <div className="flex flex-col gap-5 items-center justify-center">
+              <h2 className="text-2xl lg:text-[40px] leading-[120%] font-semibold"><span className="text-brand"> Mission</span>  , Vision & Values 
+              </h2>
+              <p className="default-list-text leading-normal! text-center">When claims stay organized, the process becomes faster and stress-free.</p>
+            </div>
+
+            {/* boxx */}
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-10 ">
+                {/* first */}
+            <div className="border border-[#DCFCE7] rounded-md px-6 lg:px-12 flex flex-col gap-2 mt-10 mb-10 items-center justify-center bg-white w-full h-full">
+                <Image
+                src={mission}
+                alt="mission"
+                width={100}
+                height={100}
+                className="w-10"
+                />
+                <p className="text-xl"> Our Mission </p>
+                 <p className="default-list-text leading-normal! text-center">When claims stay organized, the process becomes faster and stress-free.</p>
+            </div>
+            {/* first */}
+            <div className="border border-[#FEE2E2] rounded-md px-6 lg:px-12 flex flex-col gap-2 mt-10 mb-10 items-center justify-center bg-white h-full">
+                <Image
+                src={vision}
+                alt="vision"
+                width={100}
+                height={100}
+                className="w-10"
+                />
+                <p className="text-xl"> Our Vision </p>
+                 <p className="default-list-text leading-normal! text-center">To build an Advanced AI system that can reliably analyse insurance claims and explain complex claims in clear, plain language.</p>
+            </div>
+            {/* first */}
+            <div className="border border-[#FEF9C3] rounded-md px-6 lg:px-12 flex flex-col gap-2 mt-10 mb-10 items-center justify-center bg-white h-full">
+                <Image
+                src={values}
+                alt="values"
+                width={100}
+                height={100}
+                className="w-10"
+                />
+                <p className="text-xl"> Our Values </p>
+                 <p className="default-list-text leading-normal! text-center">Clarity, independence, and realism. We believe people deserve to understand where they stand.</p>
+            </div>
+          </div>
+      </div>
+
+       
+
+
       </div>
     </div>
   );
